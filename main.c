1650367f4e0d501a11738405a2749ddc05ba2437
Carlos Brito <cfabrito@gmail.com>
2019-04-02 14:29:35 +0100

    add function mult

--- main.c
+++ main.c
@@ -2,11 +2,18 @@
 
 int sum(int a, int b)
 {
-	return a + b;
+    return a + b;
+}
+
+int mult(int a, int b)
+{
+    return a * b;
 }
 
 int main(int argc, char *argv[])
 {
 	printf("Hello, reversi!");
 	printf("sum(1, 1): %d", sum(1,1));
+
+    printf("mult(2, 2): %d", mult(2, 2));
 }
